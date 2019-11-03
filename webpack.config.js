39dc5bb92e09a365327e78a9cad5c0db99fb013a
wxt <602574436@qq.com>
2019-11-03 22:27:35 +0800

    webpack打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 /************************************webpack配置***************************************/
 const path = require('path');//path模块
 
-
 module.exports = {
   entry: './src/myCesium.js',//入口文件
   output: {//打包输出配置
